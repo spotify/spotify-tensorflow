608edd1074e38a6c52f4c07f967d6d29c0e424d0
François Le Lay <fly@spotify.com>
2017-11-16 21:02:51 -0500

    add sharding for distributed execution

--- spotify_tensorflow/dataset.py
+++ spotify_tensorflow/dataset.py
@@ -21,6 +21,8 @@ from __future__ import absolute_import, division, print_function
 import multiprocessing as mp
 
 import tensorflow as tf
+import os
+import json
 from tensorflow.python.lib.io import file_io
 
 
@@ -122,6 +124,20 @@ class Datasets(object):
         num_features, parse_fn = Datasets.get_parse_proto_function(feature_desc_path,
                                                                    feature_mapping_fn)
         dataset = tf.data.Dataset.from_tensor_slices(filenames)
+
+        tf_config = os.environ.get('TF_CONFIG')
+
+        # If TF_CONFIG is not available don't bother sharding
+        if tf_config is not None:
+            tf_config_json = json.loads(tf_config)
+            tf.logging.info('we have a TF_CONFIG: %s' % tf_config)
+            num_workers = len(tf_config_json.get('cluster',{}).get('worker',[]))
+            tf.logging.info('num_workers=%s' % num_workers)
+            worker_index = tf_config_json.get('task', {}).get('index', None)
+            tf.logging.info('sharding on worker_index=%s' % worker_index)
+            if worker_index is not None:
+                dataset = dataset.shard(num_workers, worker_index)
+
         # TODO(rav): does `map` need to be inside `interleave`, what are the performance diff?
         dataset = dataset.interleave(lambda f: tf.data.TFRecordDataset(f, compression_type),
                                      cycle_length=num_threads,
