bdc279beb1c20f1a9da02da35c35617b44f4a59c
Rafal Wojdyla <rav@spotify.com>
2018-02-12 20:45:22 -0500

    Add examples - tf and sklearn

---
+++ scripts/run_examples.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+DIR_OF_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+EXAMPLES_DIR="$DIR_OF_SCRIPT/../examples"
+
+for i in $(ls $EXAMPLES_DIR); do
+  if [[ -d "$EXAMPLES_DIR/$i" && $i =~ ^example* ]] ; then
+    PYTHONPATH=$EXAMPLES_DIR/$i:$EXAMPLES_DIR:$DIR_OF_SCRIPT/..:$PYTHONPATH python -m main
+  fi
+done
