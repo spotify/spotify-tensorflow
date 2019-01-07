271a58cdc7f7eeadafd134b6ece3abcbb16ddd37
Brian Martin <brianmartin@gmail.com>
2019-01-07 11:42:55 -0500

    tfr-read: simplify implementation

--- spotify_tensorflow/scripts/tfr_read.py
+++ spotify_tensorflow/scripts/tfr_read.py
@@ -28,7 +28,47 @@ from spotify_tensorflow.example_decoders import ExampleWithFeatureSpecDecoder, E
 from spotify_tensorflow.tf_schema_utils import SchemaToFeatureSpec
 
 
-def get_args():
+def resolve_schema(dir, default_schema=None):
+    if default_schema is not None:
+        return default_schema
+
+    for schema_file_name in ["_schema.pb", "_inferred_schema.pb"]:
+        s = os.path.join(dir, schema_file_name)
+        if file_io.file_exists(s):
+            return s
+
+
+def list_tf_records(paths, default_schema):
+    for p in paths:
+        files = [f for f in file_io.get_matching_files(p) if f.endswith(".tfrecords")]
+        if len(files) == 0:
+            raise Exception("Couldn't find any .tfrecords file in path or glob [{}]".format(p))
+        for f in files:
+            yield f, resolve_schema(os.path.dirname(f), default_schema)
+
+
+def get_decoder_from_schema(schema):
+    if schema is None:
+        return ExampleDecoder()
+    else:
+        schema_object = SchemaToFeatureSpec.parse_schema_file(schema)
+        feature_spec = SchemaToFeatureSpec.apply(schema_object)
+        return ExampleWithFeatureSpecDecoder(feature_spec)
+
+
+def tfr_read_to_json(tf_records_path, schema_path=None):
+    if schema_path is not None:
+        assert file_io.file_exists(schema_path), "File not found: {}".format(schema_path)
+
+    for tf_record_file, schema in list_tf_records(tf_records_path, schema_path):
+        assert file_io.file_exists(tf_record_file), "File not found: {}".format(tf_record_file)
+
+        decoder = get_decoder_from_schema(schema)
+        for record in tf.python_io.tf_record_iterator(tf_record_file):
+            yield decoder.to_json(record)
+
+
+def main():
     parser = argparse.ArgumentParser(description="Output TFRecords as JSON")
     parser.add_argument("-s", "--schema", help="Path to Schema protobuf file. Uses Example if not "
                                                "supplied.")
@@ -36,74 +76,16 @@ def get_args():
                         metavar="TF_RECORDS_PATH",
                         nargs="+",
                         help="TFRecords file (or directory containing .tfrecords files)")
-    return parser.parse_args()
 
+    args = parser.parse_args()
 
-def resolve_dir_schemas(dirs):
-    dir_schemas = []
-    for d in dirs:
-        s = os.path.join(d, "_schema.pb")
-        s_inferred = os.path.join(d, "_inferred_schema.pb")
-        if file_io.file_exists(s):
-            dir_schemas.append((d, s))
-        elif file_io.file_exists(s_inferred):
-            dir_schemas.append((d, s_inferred))
-        else:
-            dir_schemas.append((d, None))
-    return dict(dir_schemas)
-
-
-def list_tf_records(paths, default_schema):
-    for p in paths:
-        if file_io.file_exists(p):
-            files = [p]
-        else:
-            # dir or glob
-            files = [f for f in file_io.get_matching_files(p) if f.endswith(".tfrecords")]
-
-        dirs = set(os.path.dirname(f) for f in files)
-        if default_schema:
-            dir_schemas = dict((d, default_schema) for d in dirs)
-        else:
-            dir_schemas = resolve_dir_schemas(dirs)
-
-        for f in files:
-            yield f, dir_schemas[os.path.dirname(f)]
-
-
-def main():
-    cmdline_args = get_args()
-
-    default_schema = None
-    if cmdline_args.schema:
-        default_schema = cmdline_args.schema
-        assert file_io.file_exists(default_schema), "File not found: {}".format(default_schema)
-
-    last_schema = None
-    example_decoder = ExampleDecoder()
-
-    for tf_record_file, schema in list_tf_records(cmdline_args.tf_records_paths,
-                                                  default_schema):
-        assert file_io.file_exists(tf_record_file), "File not found: {}".format(tf_record_file)
-
-        # Load the right example decoder
-        if schema != last_schema:
-            last_schema = schema
-            if schema:
-                schema_object = SchemaToFeatureSpec.parse_schema_file(schema)
-                feature_spec = SchemaToFeatureSpec.apply(schema_object)
-                example_decoder = ExampleWithFeatureSpecDecoder(feature_spec)
-            else:
-                example_decoder = ExampleDecoder()
-
-        # decode the examples
-        for record in tf.python_io.tf_record_iterator(tf_record_file):
-            try:
-                print(example_decoder.to_json(record))
-            except IOError as e:
-                if e.errno == errno.EPIPE:
-                    sys.exit(0)
-                raise e
+    try:
+        for json_str in tfr_read_to_json(args.tf_records_path, args.schema):
+            print(json_str)
+    except IOError as e:
+        if e.errno == errno.EPIPE:
+            sys.exit(0)
+        raise e
 
 
 if __name__ == "__main__":
